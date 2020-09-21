a4aa79cd1e072339235dbb28f67283bff09a9c97
JongHyeonSong <mapelamll@naver.com>
2020-09-21 19:24:17 +0900

    Updates

---
+++ precache-manifest.cafe6a68ec99eb352e5b09435dab02b8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e4e2a210eb7bfed8699f6a624811bce",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7afeb072f8677c17cf82",
+    "url": "/portfolio/static/css/2.ca9837c0.chunk.css"
+  },
+  {
+    "revision": "3dabb6d029ab3cf79a81",
+    "url": "/portfolio/static/css/main.7c7797ca.chunk.css"
+  },
+  {
+    "revision": "7afeb072f8677c17cf82",
+    "url": "/portfolio/static/js/2.1d36e6ba.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/portfolio/static/js/2.1d36e6ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dabb6d029ab3cf79a81",
+    "url": "/portfolio/static/js/main.002d0521.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "07ba63edb7ef41cde3d74ceeebaf62d1",
+    "url": "/portfolio/static/media/commers.07ba63ed.png"
+  },
+  {
+    "revision": "538857381de5e62de9db05348d75f497",
+    "url": "/portfolio/static/media/coreup.53885738.png"
+  },
+  {
+    "revision": "587c9a1cc2381dcf4f0981c69e981688",
+    "url": "/portfolio/static/media/isutube.587c9a1c.png"
+  },
+  {
+    "revision": "2fe208654793a3d18a44869a7565c52c",
+    "url": "/portfolio/static/media/react_todo.2fe20865.png"
+  },
+  {
+    "revision": "b55d7f55357c7716af62e017e6a2fdd3",
+    "url": "/portfolio/static/media/sjh.b55d7f55.png"
+  }
+]);
